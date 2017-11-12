0512aa81fab34f359b21e7de10c76101c7f0fe28
Miki Manor <mikimanor@gmail.com>
2017-11-12 17:13:51 +0000

    update ex3.sh

--- bashScripts/ex3.sh
+++ bashScripts/ex3.sh
@@ -5,6 +5,12 @@ arg2=$2
 
 if [ ${#arg1} -gt 3 ] && [ ${#arg2} -gt 3 ]; then
 	echo All Good
+	if [ ${arg1} == ${arg2} ]; then
+		echo both args are equal
+	else
+		echo args are different
+	fi
 else
 	echo Not good
+	exit 3
 fi
